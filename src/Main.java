7b28f41d6f85a9345074ce454650350edb20a825
René Hahn <renehahn@Renes-MBP.fritz.box>
2018-12-03 18:21:22 +0100

    This is my second commit in my Practice01 repository

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-
+    // This is a leading comment
+    // This is another comment
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
